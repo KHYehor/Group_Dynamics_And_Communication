e9c98ee50b7d610c2a957a894acd5e3489e06faf
ykh2k <khilchenko.yehor@gmail.com>
2020-10-04 02:43:27 +0300

    feat: init project

---
+++ deploy/prod.sh
@@ -0,0 +1,13 @@
+if test -d backup/Group_Dynamics_And_Communication; then
+  rm -r backup/Group_Dynamics_And_Communication;
+fi
+
+mv Group_Dynamics_And_Communication/ backup/;
+
+# Link here
+git clone  &&
+
+cd Group_Dynamics_And_Communication &&
+
+yarn install &&
+
