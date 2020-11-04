db0b4b3c6c1acd0c362f099a6b3336520064cb32
ykh2k <khilchenko.yehor@gmail.com>
2020-11-05 00:04:50 +0200

    feat: first migrations

---
+++ src/migration/1604514126693-GroupsMigrations.ts
@@ -0,0 +1,21 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class GroupsMigrations1604514126693 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        return queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS Groups(
+                id              SERIAL  NOT NULL PRIMARY KEY,
+                university_id   SERIAL  NOT NULL REFERENCES Universities(id) ON DELETE CASCADE
+                name            text    NOT NULL UNIQUE 
+            );
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        return queryRunner.query(`
+            DROP TABLE Groups IF EXISTS;
+        `);
+    }
+
+}
